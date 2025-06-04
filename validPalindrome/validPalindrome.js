a0811edad6dd2176cfedcc29c771a7f174200991
Raisa Dorzback <raisa.dorzback@gmail.com>
2025-06-04 14:20:46 -0400

    june 4

--- validPalindrome/validPalindrome.js
+++ validPalindrome/validPalindrome.js
@@ -15,3 +15,43 @@ class Solution {
         return reversed === s;
     };
 };
+
+// Two pointer
+// Time: O(n)
+// Space: O(1)
+class Solution {
+    isAlphanum(char) {
+        const alphanum = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        return alphanum.includes(char.toLowerCase());
+    };
+
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(str) {
+        // create pointers
+        let left = 0, right = str.length - 1;
+
+        // while pointers haven't met/crossed each other yet
+        while(left < right) {
+            // check if both chars are alphanum and haven't crossed
+            while(left < right && !this.isAlphanum(str[left])) {
+                left++;
+            };
+            while(right > left && !this.isAlphanum(str[right])) {
+                right--;
+            };
+
+            // if left and right are not equal, they aren't a palindrome
+            if(str[left].toLowerCase() !== str[right].toLowerCase()) {
+                return false;
+            };
+
+            // update pointers
+            left++;
+            right--;
+        };
+        return true;
+    };
+};
